3e653f9c0d9fcb1cfd8b679072b79e181072406f
Tomas Pinto <tomas.pinto@spark.net>
2021-02-25 18:05:00 +0100

    WIP: Add restful controller to main

--- Sermo.go
+++ Sermo.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
-	w "sermo/word"
+	controller "sermo/controller"
+	web "sermo/web"
 )
 
 func main() {
 
-	newWord := w.CreateWord("label", "meaning")
-
+	/*newWord := w.CreateWord("label", "meaning", "")
 	fmt.Printf("This is the new word %v wich means %v", newWord.Label, newWord.Meaning)
-
 	repo := w.NewRepository()
-
 	_ = repo.AddWord(newWord)
-
 	result := repo.ListWords()
+	fmt.Printf("This is the new word %v wich means %v ", result[0].Label, result[0].Meaning)*/
 
-	fmt.Printf("This is the new word %v wich means %v ", result[0].Label, result[0].Meaning)
+	fmt.Println("Server starting ")
+
+	router := web.NewRouter()
+
+	greetings := controller.NewGreetings()
+	router.AddPath(greetings)
+	router.RouterStart()
+
 }
