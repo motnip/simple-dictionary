f59ecdc4945cc0ddfb294c7c80688774ed4b8e90
Tomas Pinto <tomas.pinto@spark.net>
2021-02-27 17:42:52 +0100

    Delet dead code

--- word/word.go
+++ word/word.go
@@ -5,11 +5,3 @@ type Word struct {
 	Meaning  string
 	Sentence string
 }
-
-func CreateWord(label string, meaning string, sentence string) *Word {
-	return &Word{
-		Label:    label,
-		Meaning:  meaning,
-		Sentence: sentence,
-	}
-}
