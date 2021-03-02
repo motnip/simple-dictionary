00b91d27e8ad5d4612a0e4dabe213c0b22c60246
Tomas Pinto <tomas.pinto@spark.net>
2021-03-02 10:51:38 +0100

    Add return http code 201 when create dictionary

--- controller/controller.go
+++ controller/controller.go
@@ -1,22 +1,21 @@
 package controller
 
-
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Controllers interface {
-    CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request)
+	CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request)
 }
 
-
-func  CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request) {
-    reqBody, err := ioutil.ReadAll(httpRequest.Body)
-    if err != nil {
-        fmt.Fprintf(httpResponse, "Kindly enter data with the event title and description only in order to update")
-    }
-    input := string(reqBody[:])
-    fmt.Fprintf(httpResponse, input)
+func CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+	reqBody, err := ioutil.ReadAll(httpRequest.Body)
+	if err != nil {
+		fmt.Fprintf(httpResponse, "Kindly enter data with the event title and description only in order to update")
+	}
+	httpResponse.WriteHeader(http.StatusCreated)
+	input := string(reqBody[:])
+	fmt.Fprintf(httpResponse, input)
 }
--- controller/controller_test.go
+++ controller/controller_test.go
@@ -1,30 +1,30 @@
 package controller
 
 import (
-    "bytes"
-    "net/http/httptest"
-    "testing"
-    "net/http"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestCreateDictionary(t *testing.T) {
 
-    dictionaryLanguage := "language"
-    request, err := http.NewRequest("GET", "/dictionary", bytes.NewBuffer([]byte(dictionaryLanguage)))
-    if err != nil {
-        t.Fatal(err)
-    }
+	dictionaryLanguage := "language"
+	request, err := http.NewRequest("GET", "/dictionary", bytes.NewBuffer([]byte(dictionaryLanguage)))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    recorder := httptest.NewRecorder()
-    http.HandlerFunc(CreateDictionary).ServeHTTP(recorder, request)
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(CreateDictionary).ServeHTTP(recorder, request)
 
-    if status := recorder.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",status, http.StatusOK)
-    }
+	if status := recorder.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusCreated)
+	}
 
-    responseBody := recorder.Body.String()
-    if responseBody != dictionaryLanguage {
-        t.Errorf("handler returned unexpected body: got %v want %v",responseBody, dictionaryLanguage)
-    }
+	responseBody := recorder.Body.String()
+	if responseBody != dictionaryLanguage {
+		t.Errorf("handler returned unexpected body: got %v want %v", responseBody, dictionaryLanguage)
+	}
 
 }
