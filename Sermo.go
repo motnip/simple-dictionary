7609ff330228ef3351144a9ec4918af02e1a8125
Tomas Pinto <tomas.pinto@spark.net>
2021-03-03 18:56:34 +0100

    Clean main file by dead code

--- Sermo.go
+++ Sermo.go
@@ -2,27 +2,10 @@ package main
 
 import (
 	"fmt"
-	"sermo/controller"
-	"sermo/web"
 )
 
 func main() {
 
 	fmt.Println("Server starting ")
 
-	router := web.NewRouter()
-
-	goodBy := controller.NewGoodbye()
-	router.AddPath(&web.Route{
-		Path:           goodBy.Path(),
-		ControllerFunc: goodBy.Goodbye,
-	})
-
-	greetings := controller.NewGreetings()
-	router.AddPath(&web.Route{
-		Path:           greetings.Path(),
-		ControllerFunc: greetings.Greetings,
-	})
-
-	router.RouterStart()
 }
