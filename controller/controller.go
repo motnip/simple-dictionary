2c3de56667eaab8b8f57e7eba2917bd8027fef72
Tomas Pinto <tomas.pinto@spark.net>
2021-03-03 18:56:15 +0100

    Return bad request if dictionary already exists

--- controller/controller.go
+++ controller/controller.go
@@ -16,20 +16,25 @@ type controller struct {
 	repository model.Repository
 }
 
-func NewController(repository model.Repository) Controllers{
+func NewController(repository model.Repository) Controllers {
 	return &controller{
 		repository: repository,
 	}
 }
-func (c *controller)CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+func (c *controller) CreateDictionary(httpResponse http.ResponseWriter, httpRequest *http.Request) {
+	var input string
 	reqBody, err := ioutil.ReadAll(httpRequest.Body)
 	if err != nil {
 		fmt.Fprintf(httpResponse, "Kindly enter data with the event title and description only in order to update")
 	}
+
+	if input = string(reqBody[:]); input == "" {
+		httpResponse.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(httpResponse, input)
+		return
+	}
+
 	httpResponse.WriteHeader(http.StatusCreated)
-	input := string(reqBody[:])
-
 	c.repository.CreateDictionary(input)
-
 	fmt.Fprintf(httpResponse, input)
 }
--- controller/controller_test.go
+++ controller/controller_test.go
@@ -5,8 +5,8 @@ import (
 	"github.com/golang/mock/gomock"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 	"sermo/mocks/repository"
+	"testing"
 )
 
 func TestCreateDictionary(t *testing.T) {
@@ -36,6 +36,38 @@ func TestCreateDictionary(t *testing.T) {
 
 	responseBody := recorder.Body.String()
 	if responseBody != dictionaryLanguage {
+		t.Errorf("handler returned unexpected body: got %v want %v", responseBody, dictionaryLanguage)
+	}
+
+}
+
+func TestCreateDictionary_noLanguage_returnBadRequest(t *testing.T) {
+	//given
+	controller := gomock.NewController(t)
+	repositoryMock := mock_model.NewMockRepository(controller)
+
+	sut := NewController(repositoryMock)
+
+	dictionaryLanguage := ""
+	request, err := http.NewRequest("GET", "/dictionary", bytes.NewBuffer([]byte(dictionaryLanguage)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	//when
+	repositoryMock.EXPECT().CreateDictionary(gomock.Any()).Times(0)
+
+	http.HandlerFunc(sut.CreateDictionary).ServeHTTP(recorder, request)
+
+	//then
+	if status := recorder.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+
+	responseBody := recorder.Body.String()
+	if responseBody != "" {
 		t.Errorf("handler returned unexpected body: got %v want %v", responseBody, dictionaryLanguage)
 	}
 
