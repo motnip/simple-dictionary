1be13640e6aeb87c95d4924fb62e6e3a4f535730
Tomas Pinto <tomas.pinto@spark.net>
2021-03-17 09:46:20 +0100

    Inject depedencies to the router

--- Sermo.go
+++ Sermo.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sermo/controller"
+	"sermo/model"
 	"sermo/web"
 )
 
 func main() {
 
-	fmt.Println("Server starting ")
+	fmt.Println("Server starting... ")
 
-	router := web.NewRouter()
+	repository := model.NewRepository()
+	controller := controller.NewController(repository)
+
+	router := web.NewRouter(controller)
 	router.RouterStart()
 }
--- web/router.go
+++ web/router.go
@@ -1,22 +1,22 @@
 package web
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	c "sermo/controller"
-	"sermo/model"
 )
 
 type Router struct {
-	router *mux.Router
+	router     *mux.Router
 	controller c.Controllers
 }
 
-func NewRouter() *Router {
+func NewRouter(controller c.Controllers) *Router {
 	return &Router{
-		router: mux.NewRouter().StrictSlash(true),
-		controller: c.NewController(model.NewRepository()),
+		router:     mux.NewRouter().StrictSlash(true),
+		controller: controller,
 	}
 }
 
@@ -24,5 +24,6 @@ func (r *Router) RouterStart() {
 	r.router.HandleFunc("/dictionary", r.controller.CreateDictionary)
 	r.router.HandleFunc("/word", r.controller.AddWord)
 	r.router.HandleFunc("/word", r.controller.ListWords).GetMethods()
+	fmt.Println("Server started... ")
 	log.Fatal(http.ListenAndServe(":8080", r.router))
 }
