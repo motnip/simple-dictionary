55a6e81178c58d8c46339a3940e0d6202a6173a9
motnip <tomas.pinto@spark.net>
2021-10-30 16:19:05 +0200

    Change to Testify

--- model/repository_test.go
+++ model/repository_test.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
@@ -11,77 +12,80 @@ var newWord = Word{
 	Meaning: "ciao",
 }
 
-func TestCreateDictionary(t *testing.T) {
+type RepositoryTestSuite struct {
+	suite.Suite
+	repo Repository
+}
 
-	repo := NewRepository()
+func TestWordTestSuite(t *testing.T) {
+	suite.Run(t, new(RepositoryTestSuite))
+}
+
+func (r *RepositoryTestSuite) SetupTest() {
+	r.repo = NewRepository()
+}
+
+func (r *RepositoryTestSuite) TestCreateDictionary() {
 
 	dictionary := &Dictionary{
 		Language: "en",
 	}
 
-	result, err := repo.CreateDictionary("en")
+	result, err := r.repo.CreateDictionary("en")
 
-	assert.NoError(t, err)
-	assert.Equal(t, dictionary, result, "expected %v got %v", dictionary, result)
+	assert.NoError(r.T(), err)
+	assert.Equal(r.T(), dictionary, result, "expected %v got %v", dictionary, result)
 }
 
-func TestCreateDictionary_dictionaryAlreadyExists_Fail(t *testing.T) {
-
-	repo := NewRepository()
+func (r *RepositoryTestSuite) TestCreateDictionary_dictionaryAlreadyExists_Fail() {
 
 	language := "en"
 	expectedErroMsg := fmt.Errorf("dictionary %s already exists", language)
-	_, err := repo.CreateDictionary("en")
-	assert.NoError(t, err)
+	_, err := r.repo.CreateDictionary("en")
+	assert.NoError(r.T(), err)
 
-	secondDictionaryResult, err := repo.CreateDictionary("en")
-	assert.Error(t, err, "expected %v got %v", expectedErroMsg, secondDictionaryResult)
-	assert.EqualError(t, expectedErroMsg, err.Error(), "expected %v got %v", expectedErroMsg, secondDictionaryResult)
+	secondDictionaryResult, err := r.repo.CreateDictionary("en")
+	assert.Error(r.T(), err, "expected %v got %v", expectedErroMsg, secondDictionaryResult)
+	assert.EqualError(r.T(), expectedErroMsg, err.Error(), "expected %v got %v", expectedErroMsg, secondDictionaryResult)
 
 }
 
-func TestListDictionary(t *testing.T) {
+func (r *RepositoryTestSuite) TestListDictionary() {
 
-	repo := NewRepository()
+	_, _ = r.repo.CreateDictionary("en")
 
-	_, _ = repo.CreateDictionary("en")
+	result := r.repo.ListDictionary()
 
-	result := repo.ListDictionary()
-
-	assert.Equal(t, 1, len(result))
+	assert.Equal(r.T(), 1, len(result))
 }
 
-func TestAddWord(t *testing.T) {
+func (r *RepositoryTestSuite) TestAddWord() {
 
-	repo := NewRepository()
-	repo.CreateDictionary("en")
-	err := repo.AddWord(&newWord)
+	r.repo.CreateDictionary("en")
+	err := r.repo.AddWord(&newWord)
 
-	assert.NoError(t, err)
+	assert.NoError(r.T(), err)
 }
 
-func TestExistsDictionary_Succeed(t *testing.T) {
+func (r *RepositoryTestSuite) TestExistsDictionary_Succeed() {
 
-	repo := NewRepository()
-	repo.CreateDictionary("en")
+	r.repo.CreateDictionary("en")
 
-	result := repo.ExistsDictionary()
+	result := r.repo.ExistsDictionary()
 
-	assert.True(t, result)
+	assert.True(r.T(), result)
 }
 
-func TestExistsDictionary_noDictionaryAvailable_Failed(t *testing.T) {
+func (r *RepositoryTestSuite) TestExistsDictionary_noDictionaryAvailable_Failed() {
 
-	repo := NewRepository()
-	result := repo.ExistsDictionary()
+	result := r.repo.ExistsDictionary()
 
-	assert.False(t, result)
+	assert.False(r.T(), result)
 }
 
-func TestListWord(t *testing.T) {
+func (r *RepositoryTestSuite) TestListWord() {
 
-	repo := NewRepository()
-	repo.CreateDictionary("en")
+	r.repo.CreateDictionary("en")
 
 	sentence := "Hello world!"
 	newWord := Word{
@@ -90,13 +94,13 @@ func TestListWord(t *testing.T) {
 		Sentence: sentence,
 	}
 
-	err := repo.AddWord(&newWord)
-	assert.NoError(t, err)
+	err := r.repo.AddWord(&newWord)
+	assert.NoError(r.T(), err)
 
-	result, _ := repo.ListWords()
+	result, _ := r.repo.ListWords()
 
-	assert.GreaterOrEqualf(t, 1, len(result), "no list of words have been returned", result)
-	assert.Equal(t, newWord.Label, result[0].Label)
-	assert.Equal(t, newWord.Meaning, result[0].Meaning)
-	assert.Equal(t, newWord.Sentence, result[0].Sentence)
+	assert.GreaterOrEqualf(r.T(), 1, len(result), "no list of words have been returned", result)
+	assert.Equal(r.T(), newWord.Label, result[0].Label)
+	assert.Equal(r.T(), newWord.Meaning, result[0].Meaning)
+	assert.Equal(r.T(), newWord.Sentence, result[0].Sentence)
 }
